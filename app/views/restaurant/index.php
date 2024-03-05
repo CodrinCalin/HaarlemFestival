8c632b6a50fec5b99ac030699aaedfbd9c982df4
jonathan-mauricio <jonathanmauricionl@gmail.com>
2024-03-05 16:52:57 +0100

    Basic yummy page setup

--- app/views/restaurant/index.php
+++ app/views/restaurant/index.php
@@ -11,17 +11,29 @@ include __DIR__ . "/../header.php";
     <link href="/css/restaurantstyle.css" rel="stylesheet">
 </head>
 
-<div class="front-page">
-    <div class="food-bg-darkening"></div>
-    <img class="foodbg" src="/img/png/food-bg.png" alt="Showing a group of people at a food festival">
-    <div class="yummy-text-bg"></div>
-    <div class="main-text">
-        <h1>Yummy!</h1>
+<body>
+
+<div class="container-fluid p-0 m-0">
+    <img src="/img/png/food-bg.png" alt="People at a festival">
+    <div class="yummy-text position-absolute top-50 start-50 px-5">Yummy!</div>
+    <div class="date-text position-absolute fw-bold top-50 start-50">July 25th-28th</div>
+    <div class="caption-text position-absolute fw-bold top-50 start-50 text-center vw-100">A Food Festival in Haarlem</div>
+
+    <div class="details-bg p-4 vw-100">
+        <div class="yummy-details text-black fw-bold position-relative text-center vw-100">Yummy! Food Event Details</div>
+        <div class="yummy-details-text text-black position-relative text-center vw-50 mt-2 mb-2">Come and see the participating restaurants at our very own food event at the Haarlem Festival. Featuring all sorts of different cuisines you're sure to find something you that fits your tastes! Take a quick look at each restaurant and easily find out more about any restaurant and book your very own reservation by clicking "Learn More".</div>
+        <div class="yummy-details-text text-blue fw-bold position-relative text-center vw-50">REMINDER:</div>
+        <div class="yummy-details-text text-blue fw-bold position-relative text-center vw-50">A reservation is mandatory to dine at participating restaurants, remember to book before you wish to dine!</div>
     </div>
-    <div class="sub-text">
-        <h3>July 27th-28th</h3>
-    </div>
+
+    <div class="banner text-black fw-bold position-relative text-center mt-3">From The Sea</div>
+    <!--            Insert Seafood Cards Here-->
+    <div class="banner text-black fw-bold position-relative text-center mt-3 text-nowrap">International Cuisine</div>
+    <!--            Insert International Cards Here-->
+
 </div>
+
+</body>
 
 
 
