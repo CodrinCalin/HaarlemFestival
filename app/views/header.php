8dcc2dd9deccfb4fef09bc98a235e653eff212b8
IgnasMon <95746420+IgnasMon@users.noreply.github.com>
2024-03-25 08:58:06 +0100

    Merge Conflicts Resolved
    Resolved the current footer, header and style.css file conflict to merge.

--- app/views/header.php
+++ app/views/header.php
@@ -12,24 +12,55 @@
 </style>
     <main>
         <div class="bg-secondary">
-            <header class="d-flex justify-content-center py-3">
-                <ul class="nav nav-pills">
-                    <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="/history" class="nav-link">History</a></li>
-                    <li class="nav-item"><a href="/restaurant" class="nav-link">Yummy!</a></li>
-                    <li class="nav-item"><a href="/dance" class="nav-link">Dance!</a></li>
-                    <?php
-                    if(isset($_SESSION["authUser"])) {
-                        $authUser = $_SESSION["authUser"];
-                        if($authUser->permissions == 2) { ?>
-                            <li class="nav-item"><a href="/manageusers" class="nav-link">Manage Users</a></li>
-                            <li class="nav-item"><a href="/managerestaurants" class="nav-link">Manage Restaurants</a></li>
-                        <?php }?>
-                        <li class="nav-item"><a href="/auth/logout" class="nav-link">Log out</a></li>
-                    <?php } else{ ?>
-                        <li class="nav-item"><a href="/auth" class="nav-link">Login</a></li>
-                        <li class="nav-item"><a href="/auth/register" class="nav-link">Register</a></li>
-                    <?php } ?>
-                </ul>
-            </header>
+            <div id="topNav" class="container">
+                <nav class="navbar">
+                    <a class="navbar-brand" href="/">
+                        <img class="d-inline-block align-top logo"
+                             src="\img\logo\logoFullLight.png" alt="Logo Image">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item <?php echo ($_SERVER['REQUEST_URI'] == '/') ? 'bold-nav-link' : ''; ?>">
+                            <a href="/" class="nav-link">Home</a></li>
+                        <li class="nav-item <?php echo ($_SERVER['REQUEST_URI'] == '/history') ? 'bold-nav-link' : ''; ?>">
+                            <a href="/history" class="nav-link">History</a></li>
+                        <li class="nav-item <?php echo ($_SERVER['REQUEST_URI'] == '/restaurant') ? 'bold-nav-link' : ''; ?>">
+                            <a href="/restaurant" class="nav-link">Yummy!</a></li>
+                        <li class="nav-item <?php echo ($_SERVER['REQUEST_URI'] == '/dance') ? 'bold-nav-link' : ''; ?>">
+                            <a href="/dance" class="nav-link">Dance!</a></li>
+                        <li class="nav-item">
+                            <a href="/personalprogram" class="nav-link">
+                                <img class="d-inline-block align-top nav_icon"
+                                     src="\img\icons\shoppingCart.png" alt="Personal Program">
+                            </a>
+                        </li>
+                        <?php
+                        if(isset($_SESSION["authUser"])) { ?>
+                        <?php
+                            $authUser = $_SESSION["authUser"];
+                            if($authUser->permissions < 2) { ?>
+                                <li class="nav-item dropdown <?php echo ($_SERVER['REQUEST_URI'] == '/manage') ? 'bold-nav-link' : ''; ?>">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Management
+                                    </a>
+                                    <div id="dropdownMenu" class="dropdown-menu dropdown-menu-end"
+                                         aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="/manageusers">Manage Users</a>
+                                        <a class="dropdown-item" href="/managetickets">Manage Tickets</a>
+                                    </div>
+                                </li>
+                            <?php }?>
+                            <li class="nav-item">
+                                <a href="/auth/logout" class="nav-link">
+                                    <img class="d-inline-block align-top avatarIcon"
+                                         src="\img\userAvatar\default.png" alt="Logo Image">
+                                    <?php { $authUser->username; } ?>
+                                </a>
+                            </li>
+                        <?php } else{ ?>
+                            <li class="nav-item <?php echo ($_SERVER['REQUEST_URI'] == '/auth') ? 'bold-nav-link' : ''; ?>"><a href="/auth" class="nav-link">Sign in</a></li>
+                        <?php } ?>
+                    </ul>
+                </nav>
+            </div>
         </div>
